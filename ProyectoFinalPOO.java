6083fada2376cbdacfde24007821d9509d1b3565
ZulmyRodrguez <74334021+ZulmyRodrguez@users.noreply.github.com>
2020-12-11 21:00:49 -0600

    Add files via upload

---
+++ ProyectoFinalPOO.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectofinalpoo;
+import proyectofinalpoo.Caja;
+/**
+ *
+ * @author Administrador
+ */
+public class ProyectoFinalPOO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   Caja ver =new Caja();
+   ver.setVisible(true);
+    }
+    
+}
